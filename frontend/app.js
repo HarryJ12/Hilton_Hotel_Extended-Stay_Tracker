26556e8721137c3c49cb6487f308cb5a5b3012cc
Hari Joshi <hjoshi@Mac.lan>
2026-01-03 13:29:23 -0500

    Add frontend UI (HTML and JS)

---
+++ frontend/app.js
@@ -0,0 +1,60 @@
+async function loadGuests() {
+  const res = await fetch("http://localhost:8080/guests");
+  const guests = await res.json();
+
+  const list = document.getElementById("guestList");
+  list.innerHTML = "";
+
+  guests.forEach(g => {
+    const li = document.createElement("li");
+
+  li.innerHTML = `
+    <strong>${g.name}</strong> (Room ${g.room_number})<br>
+    <span>Contact:</span> ${g.contact}<br>
+    <span>Check-In:</span> ${g.check_in_date.split("T")[0]}<br>
+    <span>Last Reminder:</span> ${g.last_reminder ?? "N/A"}<br>
+  `;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.style.marginTop = "3px";  
+
+    delBtn.onclick = async () => {
+      const res = await fetch(`http://localhost:8080/guests/${g.id}`, {
+        method: "DELETE"
+      });
+
+      if (!res.ok) {
+        alert("Delete failed");
+        return;
+      }
+
+      loadGuests();
+    };
+
+    li.appendChild(delBtn);
+    list.appendChild(li);
+  });
+}
+
+document.getElementById("guestForm").addEventListener("submit", async e => {
+  e.preventDefault();
+
+  await fetch("http://localhost:8080/guests", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      contact: document.getElementById("contact").value,
+      room_number: document.getElementById("room").value,
+      monthly_rate: parseInt(document.getElementById("rate").value, 10),
+      check_in_date: document.getElementById("checkin").value,
+      last_reminder: null
+    })
+  });
+
+  e.target.reset();
+  loadGuests();
+});
+
+loadGuests();
